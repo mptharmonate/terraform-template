d09f6bd0d3f5c0db58cdb72f9a2c6e1d5e58309d
mptsolutioning <167113569+mptsolutioning@users.noreply.github.com>
2024-06-16 10:10:27 -0700

    working

--- {{cookiecutter.project_slug}}/main.tf
+++ {{cookiecutter.project_slug}}/main.tf
@@ -3,12 +3,12 @@ data "aws_caller_identity" "current" {}
 
 # Retrieve the SSM parameter for the error alert list
 data "aws_ssm_parameter" "error_alert_list" {
-  name = "/sns/lists/error_alert_list"
+  name = "/sns/lists/${local.environment}/error_alert_list"
 }
 
 # Retrieve the SSM parameter for the info alert list
 data "aws_ssm_parameter" "info_alert_list" {
-  name = "/sns/lists/info_alert_list"
+  name = "/sns/lists/${local.environment}/info_alert_list"
 }
 
 locals {
--- {{cookiecutter.project_slug}}/variables.tf
+++ {{cookiecutter.project_slug}}/variables.tf
@@ -1,31 +1,26 @@
 variable "project_name" {
   description = "Name of the project"
   type        = string
-  default     = "{{ cookiecutter.project_name }}"
 }
 
 variable "region" {
   description = "AWS Region"
   type        = string
-  default     = "{{ cookiecutter.region }}"
 }
 
 variable "jira_ticket" {
   description = "Jira Ticket"
   type        = string
-  default     = "{{ cookiecutter.jira_ticket }}"
 }
 
 variable "environment" {
   description = "Environment"
   type        = string
-  default     = "{{ cookiecutter.environment }}"
 }
 
 variable "test_email" {
   description = "Test Email"
   type        = string
-  default     = "{{ cookiecutter.test_email }}"
 }
 
 variable "ignore_changes_on_destroy" {
