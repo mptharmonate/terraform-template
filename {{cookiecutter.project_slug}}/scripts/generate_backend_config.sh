a5a2f122e408d9abfb97fa529031d1e03ec7f06b
mptsolutioning <167113569+mptsolutioning@users.noreply.github.com>
2024-06-17 13:55:08 -0700

    fix dynamo typo

--- {{cookiecutter.project_slug}}/scripts/generate_backend_config.sh
+++ {{cookiecutter.project_slug}}/scripts/generate_backend_config.sh
@@ -4,7 +4,7 @@
 
 BUCKET="$TF_VAR_project_name-$TF_VAR_environment-tf-state"
 KEY="$TF_VAR_environment/$TF_VAR_project_name-remote-state.tfstate"
-TABLE="$TF_VAR_project_name-$TF_VAR_environment-tf-state-locks"
+TABLE="$TF_VAR_project_name-$TF_VAR_environment-tf-locks"
 REGION="$TF_VAR_region"
 
 # Replace placeholders in the template
