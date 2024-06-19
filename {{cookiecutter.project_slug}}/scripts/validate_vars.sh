275e6eac1f5a4ed47eaa6297c3cee2b2518b8c10
mptsolutioning <167113569+mptsolutioning@users.noreply.github.com>
2024-06-19 11:58:13 -0700

    add var validation

--- {{cookiecutter.project_slug}}/scripts/validate_vars.sh
+++ {{cookiecutter.project_slug}}/scripts/validate_vars.sh
@@ -9,17 +9,22 @@ fi
 TFVARS_FILE=$1
 
 # Extract variables that do not have a default value
-grep -Po 'variable "\K[^"]+' variables.tf > /tmp/all_vars.txt
-grep -Pzo 'variable "[^"]+"\s*{[^}]*default\s*=\s*[^}]*}' variables.tf | grep -Po 'variable "\K[^"]+' > /tmp/default_vars.txt
+awk '/variable/ {print $2}' variables.tf | tr -d '"' > /tmp/all_vars.txt
+awk '/variable/ {flag=1; next} /}/ {flag=0} flag && /default/ {print prev} {prev=$2}' variables.tf | tr -d '"' > /tmp/default_vars.txt
 comm -23 <(sort /tmp/all_vars.txt) <(sort /tmp/default_vars.txt) > /tmp/required_vars.txt
 
 # Print extracted required variables
-echo "Required variables without default values:"
-cat /tmp/required_vars.txt
+#echo "Required variables without default values:"
+#cat /tmp/required_vars.txt
+
+# Read defined variables from the tfvars file into an array
+DEFINED_VARS=()
+while IFS= read -r line; do
+  DEFINED_VARS+=("$line")
+done < <(awk -F '=' '{print $1}' "$TFVARS_FILE" | tr -d ' ')
 
 # Check if all required variables are defined in the tfvars file
 REQUIRED_VARS=$(cat /tmp/required_vars.txt)
-mapfile -t DEFINED_VARS < <(grep -oP '^\s*[^#]\K[^=]+' "$TFVARS_FILE" | tr -d ' ')
 
 for var in $REQUIRED_VARS; do
   if ! [[ " ${DEFINED_VARS[@]} " =~ " ${var} " ]]; then
