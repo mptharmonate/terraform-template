0cb2e3de33899b9cd16c59fec3f4967293799d54
mptsolutioning <167113569+mptsolutioning@users.noreply.github.com>
2024-07-29 15:09:56 -0700

    added terraform version

--- {{cookiecutter.project_slug}}/outputs.tf
+++ {{cookiecutter.project_slug}}/outputs.tf
@@ -4,13 +4,6 @@ output "common_tags" {
   value       = local.common_tags
 }
 
-output "ssm_parameter_names" {
-  value = [
-    data.aws_ssm_parameter.error_alert_list.name,
-    data.aws_ssm_parameter.info_alert_list.name
-  ]
-}
-
 output "aws_region" {
   value = var.region
 }
